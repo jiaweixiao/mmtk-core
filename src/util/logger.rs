969c6b2f7ace577957acf48f21dc30288790e89f
jiaweixiao <jiawxiao@gmail.com>
2023-11-25 00:13:44 +0800

    Set target of env_log output to stderr

--- src/util/logger.rs
+++ src/util/logger.rs
@@ -1,9 +1,13 @@
 use log::{self, SetLoggerError};
+use env_logger::{Builder, Env, Target};
 
 /// Attempt to init a env_logger for MMTk.
 pub fn try_init() -> Result<(), SetLoggerError> {
-    env_logger::try_init_from_env(
-        // By default, use info level logging.
-        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
-    )
+    let mut builder = Builder::from_env(
+        Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
+
+    builder.target(Target::Stderr);
+
+    builder.try_init()
 }
